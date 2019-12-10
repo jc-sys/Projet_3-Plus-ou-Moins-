29e72ffb915e0c840e67570a652ebc9c1a735f1f
jc-sys <atignonjc@gmail.com>
2019-12-10 11:40:38 +0100

    Première sauvegarde

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int nombreMystere = 0, nombreEntre = 0,nbrjoueur1 = 0,nbrjoueur2 = 0;
+int MAX, MIN ;
+int compteur=0;
+int contiparti=1;
+int mode;
+int niveau;
+// Génère un nombre aléatoire
+srand(time(NULL));
+
+/* La boucle du programme. Elle se répète tant que
+l'utilisateur n'a pas trouvé le nombre mystère */
+printf("       ===============================================================\n");
+printf("       = =========================================================== =\n");
+printf("       = =                                                         = =\n");
+printf("       = =                  JEU DE PLUS OU MOINS                   = =\n");
+printf("       = =                                                         = =\n");
+printf("       = =========================================================== =\n");
+printf("       ===============================================================\n");
+
+
+    return 0;
+}
+
